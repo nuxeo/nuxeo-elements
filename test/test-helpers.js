0f8f213d9da1d666aed24a19058d74be849cb99a
Guillaume Renard <grenard@nuxeo.com>
2017-10-05 18:01:12 +0200

    ELEMENTS-374: adapt mocked requests in test

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -53,6 +53,12 @@ function waitChanged(el, prop, times) {
 }
 
 function login(server, loginFixture) {
+  server.respondWith('GET',
+     '/dummy/json/cmis', [
+      200,
+      {'Content-Type': 'application/json'},
+      '{}'
+  ]);
   server.respondWith(
       'POST',
       '/dummy/api/v1/automation/login',
@@ -64,7 +70,7 @@ function login(server, loginFixture) {
   );
   server.respondWith(
       'GET',
-      '/dummy/api/v1/repo/default/user/Administrator?',
+      '/dummy/api/v1/repo/default/user/Administrator',
       [
         200,
         {'Content-Type': 'application/json'},
