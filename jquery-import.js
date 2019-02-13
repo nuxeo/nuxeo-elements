893dbffe936f3b994f0b619230c4db80952f0b7e
Nelson Silva <nsilva@nuxeo.com>
2019-02-13 12:52:02 +0000

    ELEMENTS-862: remove jquery and select2

--- jquery-import.js
+++
@@ -1,8 +0,0 @@
-if (!window.jQuery) {
-  // document object of the imported HTML file
-  var ownerDocument = (document._currentScript || document.currentScript).ownerDocument;
-  var script = ownerDocument.createElement('script');
-  script.async = true;
-  script.src = '../jquery/dist/jquery.min.js';
-  ownerDocument.head.appendChild(script);
-}
