693620a3ed0b06acca4308eff1574267b316614d
Gabriel Barata <gbarata@nuxeo.com>
2019-05-28 15:48:56 +0100

    ELEMENTS-911: add waitForAttrMutation and waitForChildListMutation test helpers

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -52,6 +52,41 @@ function waitChanged(el, prop, times) {
   return waitForEvent(el, `${prop}-changed`, times).then((e) => e.detail);
 }
 
+function waitForAttrMutation(el, attr, value) {
+  return new Promise((resolve) => {
+    if (value == null || el.getAttribute(attr) !== value) {
+      const observer = new MutationObserver((mutationsList) => {
+        const mutation = mutationsList.find(
+          (m) =>
+            m.type === 'attributes' &&
+            (attr ? m.attributeName === attr : true) &&
+            (value != null ? el.getAttribute(attr) === value : true),
+        );
+        if (mutation) {
+          observer.disconnect();
+          resolve(mutation);
+        }
+      });
+      observer.observe(el, { attributes: true });
+    } else {
+      resolve();
+    }
+  });
+}
+
+function waitForChildListMutation(el) {
+  return new Promise((resolve) => {
+    const observer = new MutationObserver((mutationsList) => {
+      const mutation = mutationsList.find((m) => m.type === 'childList');
+      if (mutation) {
+        observer.disconnect();
+        resolve(mutation);
+      }
+    });
+    observer.observe(el, { childList: true });
+  });
+}
+
 function login(server, loginFixture) {
 
   server.respondWith(
