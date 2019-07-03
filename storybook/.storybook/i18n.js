989fd26dec3ebed8b583493a508e996695795c57
Samuel Fialho <sfialho@nuxeo.com>
2019-07-03 18:15:07 +0100

    ELEMENTS-990: add nuxeo-data-table stories

---
+++ storybook/.storybook/i18n.js
@@ -0,0 +1,4 @@
+import messages from '../../ui/i18n/messages.json';
+
+window.nuxeo.I18n.language = 'en';
+window.nuxeo.I18n.en = messages;
--- storybook/src/lists/icons.js
+++ storybook/src/lists/icons.js
@@ -1,5 +1,4 @@
 import { IronMeta } from '@polymer/iron-meta/iron-meta.js';
-import '@polymer/iron-icon/iron-icon.js';
 
 const { value } = new IronMeta({ type: 'iconset', key: 'nuxeo' });
 export const listOfIcons = value.getIconNames();
