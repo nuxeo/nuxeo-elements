ce832aa4f5c5d1c81a8bff8f875ece1170487610
Gabriel Barata <gbarata@nuxeo.com>
2019-09-04 17:52:12 +0100

    ELEMENTS-1012: add unit tests for nuxeo-slot and native HTML slot interop with actions menu

--- ui/test/nuxeo-actions-menu.test.js
+++ ui/test/nuxeo-actions-menu.test.js
@@ -26,45 +26,85 @@ import '../widgets/nuxeo-actions-menu.js';
 
 window.html = html; // make it available for building custom elements inline
 
-const makeMenu = async (n) => {
-  const menu = fixture(html`
-    <div style="max-width: 160px;">
-      <nuxeo-actions-menu>
-        ${[...Array(n)].map(
-          () => html`
-            <nuxeo-preview-button document='{ "entity-type": "document", "facets": [] }'></nuxeo-preview-button>
-          `,
-        )}
-      </nuxeo-actions-menu>
-    </div>
-  `);
-  await flush();
-  return menu;
-};
-
 /* eslint-disable no-unused-expressions */
 suite('<nuxeo-actions-menu>', () => {
-  function menuActions(div) {
-    return div.querySelector('nuxeo-actions-menu').querySelectorAll(':not([slot="dropdown"]):not(nuxeo-slot)');
+  function makeMenuContent(n = 5) {
+    return html`
+      ${[...Array(n)].map(
+        () => html`
+          <nuxeo-preview-button document='{ "entity-type": "document", "facets": [] }'></nuxeo-preview-button>
+        `,
+      )}
+    `;
   }
 
-  function dropdownActions(div) {
-    return div.querySelector('nuxeo-actions-menu').querySelectorAll('[slot="dropdown"]:not(nuxeo-slot)');
+  async function makeMenu(n = 5) {
+    const menu = fixture(html`
+      <div style="max-width: 160px;">
+        <nuxeo-actions-menu>
+          ${makeMenuContent(n)}
+        </nuxeo-actions-menu>
+      </div>
+    `);
+    await flush();
+    return menu;
   }
 
-  function dropdownButton(div) {
-    return div.querySelector('nuxeo-actions-menu').$.dropdownButton;
+  async function makeMenuWithNuxeoSlot(slot = 'SLOT') {
+    const menu = await fixture(html`
+      <div style="max-width: 160px;">
+        <nuxeo-actions-menu>
+          <nuxeo-slot slot="${slot}"></nuxeo-slot>
+        </nuxeo-actions-menu>
+      </div>
+    `);
+    await flush();
+    return menu;
   }
 
-  async function removeAction(div) {
-    const menu = div.querySelector('nuxeo-actions-menu');
+  async function makeNuxeoSlottedMenuContent(n = 5, slot = 'SLOT') {
+    await fixture(html`
+      <nuxeo-slot-content name="content" slot="${slot}">
+        <template>
+          ${makeMenuContent(n)}
+        </template>
+      </nuxeo-slot-content>
+    `);
+    await flush();
+  }
+
+  function actionsMenu(el) {
+    return (el.shadowRoot || el).querySelector('nuxeo-actions-menu');
+  }
+
+  function menuActions(el) {
+    // We're relying on getContentChildren because a query by `:not([slot="dropdown"]):not(nuxeo-slot)` is too generic
+    // when using a slot with a nuxeo-slot inside, thus causing some elements to be returned twice. This could be fixed
+    // with `:scope > :not([slot="dropdown"]):not(nuxeo-slot)`, but `:scope` is not supported on Edge.
+    return actionsMenu(el)
+      .getContentChildren()
+      .filter((i) => i.tagName !== 'NUXEO-SLOT');
+  }
+
+  function dropdownActions(el) {
+    return actionsMenu(el)
+      .getContentChildren('slot[name="dropdown"]')
+      .filter((i) => i.tagName !== 'NUXEO-SLOT');
+  }
+
+  function dropdownButton(el) {
+    return actionsMenu(el).$.dropdownButton;
+  }
+
+  async function removeAction(el) {
+    const menu = actionsMenu(el);
     const lastChild = menu.lastElementChild;
     menu.removeChild(lastChild);
     await flush();
   }
 
-  async function addAction(div) {
-    const menu = div.querySelector('nuxeo-actions-menu');
+  async function addAction(el) {
+    const menu = actionsMenu(el);
     const action = document.createElement('nuxeo-preview-button');
     action.document = '{ "entity-type": "document", "facets": [] }';
     menu.appendChild(action);
@@ -123,54 +163,23 @@ suite('<nuxeo-actions-menu>', () => {
     expect(dropdownButton(menu).hidden).to.be.true;
   });
 
-  test('menu with slotted content', async () => {
-    const menu = await fixture(html`
-      <div style="max-width: 160px;">
-        <nuxeo-actions-menu>
-          <nuxeo-slot slot="SLOT"></nuxeo-slot>
-        </nuxeo-actions-menu>
-      </div>
-    `);
-    await flush();
-
-    await fixture(html`
-      <nuxeo-slot-content name="content" slot="SLOT">
-        <template>
-          ${[...Array(5)].map(
-            () => html`
-              <nuxeo-preview-button document='{ "entity-type": "document", "facets": [] }'></nuxeo-preview-button>
-            `,
-          )}
-        </template>
-      </nuxeo-slot-content>
-    `);
-
-    await flush();
+  test('menu with nuxeo-slotted content', async () => {
+    const menu = await makeMenuWithNuxeoSlot();
+    await makeNuxeoSlottedMenuContent();
 
     expect(menuActions(menu).length).to.be.equal(3);
     expect(dropdownActions(menu).length).to.be.equal(2);
     expect(dropdownButton(menu).hidden).to.be.false;
   });
 
-  test('menu with unresolved slotted content', async () => {
-    const menu = await fixture(html`
-      <div style="max-width: 160px;">
-        <nuxeo-actions-menu>
-          <nuxeo-slot slot="SLOT"></nuxeo-slot>
-        </nuxeo-actions-menu>
-      </div>
-    `);
-    await flush();
+  test('menu with unresolved nuxeo-slotted content', async () => {
+    const menu = await makeMenuWithNuxeoSlot();
 
     await fixture(html`
       <nuxeo-slot-content name="content" slot="SLOT">
         <template>
           <nuxeo-test-button icon="icons:home"></nuxeo-test-button>
-          ${[...Array(5)].map(
-            () => html`
-              <nuxeo-preview-button document='{ "entity-type": "document", "facets": [] }'></nuxeo-preview-button>
-            `,
-          )}
+          ${makeMenuContent(5)}
         </template>
       </nuxeo-slot-content>
     `);
@@ -215,5 +224,41 @@ suite('<nuxeo-actions-menu>', () => {
     expect(menuActions(menu).length).to.be.equal(3);
     expect(dropdownActions(menu).length).to.be.equal(3);
     expect(dropdownButton(menu).hidden).to.be.false;
+  });
+
+  suite('compatibility with nuxeo-slots and native HTML slots', () => {
+    class CustomSlottedMenuElement extends Nuxeo.Element {
+      static get is() {
+        return 'my-custom-slotted-menu-element';
+      }
+
+      static get template() {
+        return polymer.html`
+          <div style="max-width: 160px;">
+            <nuxeo-actions-menu>
+              <slot>
+                <nuxeo-slot slot="SLOT"></nuxeo-slot>
+              </slot>
+            </nuxeo-actions-menu>
+          </div>
+        `;
+      }
+    }
+    customElements.define(CustomSlottedMenuElement.is, CustomSlottedMenuElement);
+
+    test('menu with nuxeo-slot in a native slot', async () => {
+      // nuxeo-slot is the only element right now that works in a native slot inside the actions menu,
+      // since it has dedicated logics to reallocate its children
+      await makeNuxeoSlottedMenuContent();
+      const customEl = await fixture(html`
+        <my-custom-slotted-menu-element>
+          <nuxeo-slot name="SLOT"></nuxeo-slot>
+        </my-custom-slotted-menu-element>
+      `);
+      await flush();
+      expect(menuActions(customEl).length).to.be.equal(3);
+      expect(dropdownActions(customEl).length).to.be.equal(2);
+      expect(dropdownButton(customEl).hidden).to.be.false;
+    });
   });
 });
