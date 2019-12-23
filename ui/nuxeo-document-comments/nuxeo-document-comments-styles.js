7b9258e560d8d0479b3e9b4481ba279733eccec1
Nuno Cunha <ncunha@nuxeo.com>
2019-12-23 16:28:49 +0000

    ELEMENTS-1105: comment's style dependency cleanup

--- ui/nuxeo-document-comments/nuxeo-document-comments-styles.js
+++ ui/nuxeo-document-comments/nuxeo-document-comments-styles.js
@@ -14,6 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+import '@polymer/iron-flex-layout/iron-flex-layout.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 
 /*
@@ -27,7 +28,7 @@ import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 const template = html`
   <dom-module id="nuxeo-document-comments-styles">
     <template>
-      <style include="nuxeo-styles">
+      <style>
         :host {
           display: block;
         }
