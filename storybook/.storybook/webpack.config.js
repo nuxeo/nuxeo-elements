6a04da9e4981bc7c84df330db572f47c7e812966
Gabriel Barata <gbarata@nuxeo.com>
2020-01-09 18:44:24 +0000

    ELEMENTS-1098: add support for import.meta on storybook

--- storybook/.storybook/webpack.config.js
+++ storybook/.storybook/webpack.config.js
@@ -1,10 +1,17 @@
 const { resolve } = require('path');
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.stories\.js?$/,
-    loaders: [require.resolve('@storybook/addon-storysource/loader')],
-    enforce: 'pre',
-    include: resolve('../'),
-  });
+  config.module.rules.push(
+    // fix import.meta
+    {
+      test: /\.js$/,
+      loader: require.resolve('@open-wc/webpack-import-meta-loader/webpack-import-meta-loader.js'),
+    },
+    {
+      test: /\.stories\.js?$/,
+      loaders: [require.resolve('@storybook/addon-storysource/loader')],
+      enforce: 'pre',
+      include: resolve('../'),
+    },
+  );
   return config;
 };
