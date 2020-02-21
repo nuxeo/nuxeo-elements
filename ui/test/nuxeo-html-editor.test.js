22f60d1ad6fbdde15ec6ef5b3fe7c4668e003833
Nelson Silva <nsilva@nuxeo.com>
2020-02-21 11:37:48 +0000

    ELEMENTS-1124: switch nuxeo-html-editor to Quill

---
+++ ui/test/nuxeo-html-editor.test.js
@@ -0,0 +1,52 @@
+/**
+@license
+(C) Copyright Nuxeo Corp. (http://nuxeo.com/)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import { fixture, isElementVisible, html } from '@nuxeo/nuxeo-elements/test/test-helpers';
+import '../widgets/nuxeo-html-editor.js';
+
+/* eslint-disable no-unused-expressions */
+suite('<nuxeo-html-editor>', () => {
+  test('should display the placeholder when empty', async () => {
+    const editor = await fixture(
+      html`
+        <nuxeo-html-editor></nuxeo-html-editor>
+      `,
+    );
+    expect(editor._editor.root.dataset.placeholder).to.equal('Type here...');
+  });
+
+  test('should sync the html value', async () => {
+    const editor = await fixture(
+      html`
+        <nuxeo-html-editor value="Hello"></nuxeo-html-editor>
+      `,
+    );
+    await new Promise((resolve) => {
+      editor.addEventListener('value-changed', () => resolve());
+      editor._editor.insertText(editor._editor.getLength() - 1, ' world!', 'user');
+    });
+    expect(editor.value).to.equal(`<p>Hello world!</p>`);
+  });
+
+  test('should hide the toolbar when readonly', async () => {
+    const editor = await fixture(
+      html`
+        <nuxeo-html-editor read-only></nuxeo-html-editor>
+      `,
+    );
+    expect(isElementVisible(editor._editor.getModule('toolbar').container)).to.be.false;
+  });
+});
--- ui/widgets/nuxeo-html-editor.js
+++ ui/widgets/nuxeo-html-editor.js
@@ -15,29 +15,25 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { Debouncer } from '@polymer/polymer/lib/utils/debounce.js';
+import { timeOut } from '@polymer/polymer/lib/utils/async.js';
 import '@nuxeo/nuxeo-elements/nuxeo-element.js';
-import './alloy/ckeditor-init.js';
-import './alloy/alloy-ocean.js';
+import '@nuxeo/quill/dist/quill.js';
+import './quill/quill-snow.js';
 
 {
   /**
    * `nuxeo-html-editor`
    * @memberof Nuxeo
-   * @demo demo/nuxeo-html-editor/index.html
    */
   class HTMLEditor extends Nuxeo.Element {
     static get template() {
       return html`
-        <style include="alloy-ocean">
+        <style include="quill-snow">
           #editor {
             outline: none;
             height: 100%;
             min-height: 30em;
-          }
-
-          .ae-placeholder:empty:not(:focus):before {
-            color: grey;
-            font-style: italic;
           }
 
           div#editor > * {
@@ -45,7 +41,7 @@ import './alloy/alloy-ocean.js';
           }
         </style>
 
-        <div id="editor" data-placeholder$="[[placeholder]]"></div>
+        <div id="editor"></div>
       `;
     }
 
@@ -55,121 +51,90 @@ import './alloy/alloy-ocean.js';
 
     static get properties() {
       return {
-        element: Object,
-
+        /**
+         * HTML value
+         */
         value: {
           type: String,
           notify: true,
-          observer: '_valueChanged',
         },
 
+        /**
+         * Placeholder content to be displayed when empty
+         */
         placeholder: {
           type: String,
           value: 'Type here...',
         },
 
-        readOnly: Boolean,
+        /**
+         * When set the content will be read only.
+         */
+        readOnly: {
+          type: Boolean,
+          value: false,
+          reflectToAttribute: true,
+        },
 
-        hideToolbars: Boolean,
+        _editor: {
+          type: Object,
+        },
       };
+    }
+
+    static get observers() {
+      return ['_valueChanged(value, _editor)', '_readOnlyChanged(readOnly, _editor)'];
     }
 
     static get importMeta() {
       return import.meta;
     }
 
-    connectedCallback() {
-      super.connectedCallback();
-      AlloyEditor._langResourceRequested = true; // skip loading of language file
-      const alloyI18Nscript = document.createElement('script');
-      alloyI18Nscript.async = true;
-      alloyI18Nscript.src = this.resolveUrl(`alloy/lang/alloy-editor/${CKEDITOR.config.language}.js`);
-      document.head.appendChild(alloyI18Nscript);
-      alloyI18Nscript.onload = () => {
-        setTimeout(() => {
-          this._init();
-        }, 100);
-      };
-    }
-
-    _init() {
+    ready() {
+      super.ready();
       // init editor
-      const toolbars = this.hideToolbars
-        ? {}
-        : {
-            add: {
-              buttons: ['image', 'camera', 'hline', 'table'],
-              tabIndex: 2,
-            },
-            styles: {
-              selections: [
-                {
-                  name: 'link',
-                  buttons: ['linkEdit'],
-                  test: AlloyEditor.SelectionTest.link,
-                },
-                {
-                  name: 'image',
-                  buttons: ['imageLeft', 'imageCenter', 'imageRight'],
-                  test: AlloyEditor.SelectionTest.image,
-                },
-                {
-                  name: 'text',
-                  buttons: [
-                    'styles',
-                    'bold',
-                    'italic',
-                    'underline',
-                    'strike',
-                    'paragraphLeft',
-                    'paragraphCenter',
-                    'paragraphJustify',
-                    'ul',
-                    'ol',
-                    'quote',
-                    'link',
-                    'removeFormat',
-                  ],
-                  test: AlloyEditor.SelectionTest.text,
-                },
-                {
-                  name: 'table',
-                  buttons: ['tableRow', 'tableColumn', 'tableCell', 'tableRemove'],
-                  getArrowBoxClasses: AlloyEditor.SelectionGetArrowBoxClasses.table,
-                  setPosition: AlloyEditor.SelectionSetPosition.table,
-                  test: AlloyEditor.SelectionTest.table,
-                },
-              ],
-              tabIndex: 1,
-            },
-          };
-      const editor = AlloyEditor.editable(this.$.editor, {
-        readOnly: this.readOnly,
-        uiNode: this.root,
-        toolbars,
+      const toolbar = [
+        [{ header: [1, 2, 3, 4, 5, 6, false] }], // headings
+        ['bold', 'italic', 'underline', 'strike'], // style buttons
+        [{ align: '' }, { align: 'center' }, { align: 'right' }], // text alignment
+        [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+        [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+        ['blockquote'], // blockquote
+        [{ list: 'ordered' }, { list: 'bullet' }], // lists
+        ['link', 'image'], // links and media
+        ['clean'], // remove formatting button
+      ];
+
+      const { placeholder, readOnly } = this;
+      this._editor = new Quill(this.$.editor, { theme: 'snow', modules: { toolbar }, placeholder, readOnly });
+
+      // update value on change
+      this._editor.on('text-change', () => {
+        this._debouncer = Debouncer.debounce(this._debouncer, timeOut.after(200), () => this._updateValue());
       });
-      this.element = editor.get('nativeEditor');
-      // set initial value
-      this.element.setData(this.value);
-
-      this.element.on('change', this._updateValue.bind(this));
-
-      this.element.on('actionPerformed', this._updateValue.bind(this));
     }
 
     _updateValue() {
       this._internalChange = true;
-      this.value = this.element.getData();
+      this.value = this._editor.root.innerHTML;
       this._internalChange = false;
     }
 
     _valueChanged() {
-      if (this.element && !this._internalChange) {
-        this.element.setData(this.value);
+      if (this._editor && !this._internalChange) {
+        const delta = this._editor.clipboard.convert({ html: this.value });
+        this._editor.setContents(delta, Quill.sources.USER);
+        this._editor.setSelection(0, Quill.sources.SILENT);
+      }
+    }
+
+    _readOnlyChanged() {
+      if (this._editor) {
+        this._editor.enable(!this.readOnly);
+        this._editor.getModule('toolbar').container.style.display = this.readOnly ? 'none' : '';
       }
     }
   }
 
   customElements.define(HTMLEditor.is, HTMLEditor);
-  Nuxeo.HTMLEditor = HTMLEditor;
 }
