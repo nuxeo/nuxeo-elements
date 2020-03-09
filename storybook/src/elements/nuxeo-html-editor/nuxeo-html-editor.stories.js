4f0fe671c5587f7e1068b3b5068e1a312d1b8af3
Andrea <acornaglia@nuxeo.com>
2020-03-09 17:34:48 +0100

    ELEMENTS-1142: cleanup hooks for widget stories

--- storybook/src/elements/nuxeo-html-editor/nuxeo-html-editor.stories.js
+++ storybook/src/elements/nuxeo-html-editor/nuxeo-html-editor.stories.js
@@ -2,9 +2,17 @@ import { storiesOf } from '@storybook/polymer';
 import { html } from 'lit-html';
 import '@nuxeo/nuxeo-ui-elements/widgets/nuxeo-html-editor';
 
-storiesOf('UI/nuxeo-html-editor', module).addElement(
+storiesOf('UI/nuxeo-html-editor', module).add(
   'nuxeo-html-editor',
   () => html`
-    <nuxeo-html-editor></nuxeo-html-editor>
+    <style>
+      .htmlEditor {
+        margin: 20px;
+        border: 1px solid #eee;
+      }
+    </style>
+    <div class="htmlEditor">
+      <nuxeo-html-editor></nuxeo-html-editor>
+    </div>
   `,
 );
