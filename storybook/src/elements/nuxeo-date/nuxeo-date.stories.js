d485c9268522a16541efde32d8be7c93cb5b77ec
Andrea <acornaglia@nuxeo.com>
2020-03-10 10:35:22 +0100

    ELEMENTS-1045: add nuxeo-date story to Storybook

---
+++ storybook/src/elements/nuxeo-date/nuxeo-date.stories.js
@@ -0,0 +1,13 @@
+import { html } from 'lit-html';
+import { storiesOf } from '@storybook/polymer';
+import '@nuxeo/nuxeo-ui-elements/widgets/nuxeo-date';
+import { select, date } from '@storybook/addon-knobs';
+
+storiesOf('UI/nuxeo-date', module).add('Nuxeo Date', () => {
+  const dateTime = date('Date', new Date());
+  const relativeFormat = select('Format', ['MMM D, YYYY', 'MMMM D, YYYY HH:mm', 'relative'], 'MMM D, YYYY');
+  const tooltipFormat = select('Tooltip Format', ['MMMM D, YYYY HH:mm', 'relative'], 'MMMM D, YYYY HH:mm');
+  return html`
+    <nuxeo-date datetime=${new Date(dateTime)} format="${relativeFormat}" tooltip-format="${tooltipFormat}" />
+  `;
+});
