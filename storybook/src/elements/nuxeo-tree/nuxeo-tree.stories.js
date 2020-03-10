8685be6ca311b75bdab3249a82cf5f72e9fb6ba4
Andrea <acornaglia@nuxeo.com>
2020-03-10 10:39:55 +0100

    ELEMENTS-1027: add nuxeo tree to storybook

---
+++ storybook/src/elements/nuxeo-tree/nuxeo-tree.stories.js
@@ -0,0 +1,44 @@
+import { html } from 'lit-html';
+import { storiesOf } from '@storybook/polymer';
+import '@nuxeo/nuxeo-ui-elements/nuxeo-tree/nuxeo-tree.js';
+
+const data = {
+  title: 'Home',
+  children: [
+    {
+      title: 'Kitchen',
+      children: [],
+    },
+    {
+      title: 'Bedroom',
+      children: [
+        { title: 'Bed Frames', children: [] },
+        { title: 'Mattress', children: [] },
+      ],
+    },
+  ],
+};
+
+const controller = {
+  // How to get children of a node. Returns a promise.
+  getChildren(node) {
+    return Promise.resolve(node.children);
+  },
+  isLeaf(node) {
+    return node.children.length === 0;
+  },
+};
+
+storiesOf('UI/nuxeo-tree', module).add(
+  'Default',
+  () => html`
+    <nuxeo-tree .data="${data}" .controller="${controller}">
+      <template>
+        <template class="flex" is="dom-if" if="[[!isLeaf]]">
+          <iron-icon icon="icons:chevron-right" toggle></iron-icon>
+        </template>
+        <span>[[item.title]]</span>
+      </template>
+    </nuxeo-tree>
+  `,
+);
