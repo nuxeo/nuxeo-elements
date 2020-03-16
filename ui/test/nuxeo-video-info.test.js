a7cd09004debb5bb07ed5d800ace787decd5f533
Andrea <acornaglia@nuxeo.com>
2020-03-16 17:21:48 +0100

    ELEMENTS-1111: add unit tests for video-conversions and video-info

---
+++ ui/test/nuxeo-video-info.test.js
@@ -0,0 +1,73 @@
+/**
+@license
+(C) Copyright Nuxeo Corp. (http://nuxeo.com/)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import { fixture, flush, html, isElementVisible } from '@nuxeo/nuxeo-elements/test/test-helpers';
+import '../nuxeo-video/nuxeo-video-info.js';
+import videoProperties from './resources/videoProperties';
+/* eslint-disable no-unused-expressions */
+
+suite('nuxeo-video-info', () => {
+  let element;
+
+  const document = {
+    properties: {
+      'vid:info': videoProperties['vid:info'],
+    },
+  };
+
+  test('Should display 5 slots for video info when video contains info', async () => {
+    element = await fixture(html`
+      <nuxeo-video-info .document="${document}"></nuxeo-video-info>
+    `);
+    await flush();
+    const items = element.shadowRoot.querySelectorAll('.item div');
+    const labels = element.shadowRoot.querySelectorAll('.item label');
+    expect(isElementVisible(items[0])).to.be.true;
+    expect(isElementVisible(labels[0])).to.be.true;
+    expect(items[0].innerText).to.equal(videoProperties['vid:info'].format);
+    expect(isElementVisible(items[1])).to.be.true;
+    expect(isElementVisible(labels[1])).to.be.true;
+    expect(items[1].innerText).to.equal(videoProperties['vid:info'].duration.toString());
+    expect(isElementVisible(items[2])).to.be.true;
+    expect(isElementVisible(labels[2])).to.be.true;
+    expect(items[2].innerText).to.equal(videoProperties['vid:info'].width.toString());
+    expect(isElementVisible(items[3])).to.be.true;
+    expect(isElementVisible(labels[3])).to.be.true;
+    expect(items[3].innerText).to.equal(videoProperties['vid:info'].height.toString());
+    expect(isElementVisible(items[4])).to.be.true;
+    expect(isElementVisible(labels[4])).to.be.true;
+    expect(items[4].innerText).to.equal(videoProperties['vid:info'].frameRate.toString());
+  });
+
+  test('Should display 5 empty slots when no video or video does not contain info', async () => {
+    element = await fixture(html`
+      <nuxeo-video-info></nuxeo-video-info>
+    `);
+    await flush();
+    const items = element.shadowRoot.querySelectorAll('.item div');
+    const labels = element.shadowRoot.querySelectorAll('.item label');
+    expect(isElementVisible(labels[0])).to.be.true;
+    expect(items[0].innerText).to.be.empty;
+    expect(isElementVisible(labels[1])).to.be.true;
+    expect(items[1].innerText).to.be.empty;
+    expect(isElementVisible(labels[2])).to.be.true;
+    expect(items[2].innerText).to.be.empty;
+    expect(isElementVisible(labels[3])).to.be.true;
+    expect(items[3].innerText).to.be.empty;
+    expect(isElementVisible(labels[4])).to.be.true;
+    expect(items[4].innerText).to.be.empty;
+  });
+});
---
+++ ui/test/resources/videoProperties.js
@@ -0,0 +1,57 @@
+export default {
+  'vid:transcodedVideos': [
+    {
+      name: 'WebM 480p',
+      content: {
+        name: 'sample.webm',
+        'mime-type': 'video/webm',
+        encoding: null,
+        digestAlgorithm: 'MD5',
+        digest: '046356f7764cbd67acfc4365eb106e06',
+        length: '401187',
+        data: 'videoURL',
+        appLinks: [],
+      },
+      info: {
+        duration: 10.03,
+        frameRate: 25.0,
+        streams: [
+          {
+            codec: 'vp9 (Profile 0)',
+            bitRate: 0.0,
+            type: 'Video',
+          },
+          {
+            codec: 'vorbis',
+            bitRate: 0.0,
+            streamInfo: 'Stream #0:1: Audio: vorbis, 48000 Hz, stereo, fltp (default)',
+            type: 'Audio',
+          },
+        ],
+        width: 872,
+        format: 'matroska,webm',
+        height: 480,
+      },
+    },
+  ],
+  'vid:info': {
+    duration: 10.03,
+    frameRate: 25.0,
+    streams: [
+      {
+        codec: 'vp9 (Profile 0)',
+        bitRate: 0.0,
+        type: 'Video',
+      },
+      {
+        codec: 'vorbis',
+        bitRate: 0.0,
+        streamInfo: 'Stream #0:1: Audio: vorbis, 48000 Hz, stereo, fltp (default)',
+        type: 'Audio',
+      },
+    ],
+    width: 872,
+    format: 'matroska,webm',
+    height: 480,
+  },
+};
