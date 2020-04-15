f413be60abbc8d32711167e6040bce64ada3f10f
Samuel Fialho <samuel.fialho@gmail.com>
2020-04-15 16:54:48 +0100

    ELEMENTS-1170: add collapsible feature to nuxeo-checkbox-aggregation

--- ui/nuxeo-aggregation/nuxeo-checkbox-aggregation.js
+++ ui/nuxeo-aggregation/nuxeo-checkbox-aggregation.js
@@ -23,18 +23,49 @@ import '@polymer/polymer/lib/elements/dom-if.js';
 import '@polymer/polymer/lib/elements/dom-repeat.js';
 import { I18nBehavior } from '../nuxeo-i18n-behavior.js';
 import { AggregationBehavior } from './nuxeo-aggregation-behavior.js';
+import '../widgets/nuxeo-collapsible.js';
 
 {
   /**
    * An element to interact with page provider aggregations as checkboxes.
    *
-   *     <nuxeo-page-provider provider="default_search" auto
-   *                          aggregations="{{aggregations}}"
-   *                          params="[[params]]">
+   * In its simplest form:
+   *
+   *     <nuxeo-page-provider
+   *        provider="default_search"
+   *        auto
+   *        aggregations="{{aggregations}}"
+   *        params="[[params]]"
+   *     >
    *     </nuxeo-page-provider>
    *
-   *     <nuxeo-checkbox-aggregation data="[[aggregations.dc_created_agg]]"
-   *                                 value="{{params.dc_created_agg}}">
+   *     <nuxeo-checkbox-aggregation
+   *        data="[[aggregations.dc_created_agg]]"
+   *        value="{{params.dc_created_agg}}"
+   *        label="Creation date"
+   *        >
+   *     </nuxeo-checkbox-aggregation>
+   *
+   *
+   * It can also be displayed as a collapsible element, ideal for cases with a long list of checkboxes.
+   * Also, when this is the case, it is possibe to show a specific number of checkboxes and hide the
+   * rest of them.
+   *
+         <nuxeo-page-provider
+   *        provider="default_search"
+   *        auto
+   *        aggregations="{{aggregations}}"
+   *        params="[[params]]"
+   *     >
+   *     </nuxeo-page-provider>
+   *
+   *     <nuxeo-checkbox-aggregation
+   *        data="[[aggregations.dc_created_agg]]"
+   *        value="{{params.dc_created_agg}}"
+   *        label="Creation date"
+   *        collapsible
+   *        visible-items="4"
+   *        >
    *     </nuxeo-checkbox-aggregation>
    *
    * @appliesMixin Nuxeo.I18nBehavior
@@ -48,23 +79,66 @@ import { AggregationBehavior } from './nuxeo-aggregation-behavior.js';
         <style>
           :host {
             @apply --layout-vertical;
+            transition: all 0.7s;
           }
           paper-checkbox {
-            --paper-checkbox-label-spacing: 10px;
-            margin-bottom: 5px;
+            width: 100%;
+            margin-top: 6px;
+            --paper-checkbox-label-spacing: 8px;
+          }
+          nuxeo-collapsible {
+            --nuxeo-collapsible-heading-padding: 0;
+          }
+          label {
+            @apply --nuxeo-label;
+          }
+          .showButton a {
+            display: block;
+            margin-top: 4px;
           }
         </style>
 
-        <dom-repeat items="{{buckets}}">
-          <template>
-            <paper-checkbox noink checked="{{item.checked}}" on-change="_computeValues">
-              [[item.label]] ([[item.docCount]])
-            </paper-checkbox>
-          </template>
-        </dom-repeat>
         <dom-if if="[[_isEmpty]]">
           <template>
-            <label>[[i18n('checkboxAggregation.noResults')]]</label>
+            <label>[[label]]</label>
+            <span>[[i18n('checkboxAggregation.noResults')]]</span>
+          </template>
+        </dom-if>
+
+        <dom-if if="[[!_isEmpty]]">
+          <template>
+            <dom-if if="[[!collapsible]]">
+              <template>
+                <label>[[label]]</label>
+                <dom-repeat items="{{buckets}}">
+                  <template>
+                    <paper-checkbox noink checked="{{item.checked}}" on-change="_computeValues">
+                      [[item.label]] ([[item.docCount]])
+                    </paper-checkbox>
+                  </template>
+                </dom-repeat>
+              </template>
+            </dom-if>
+
+            <dom-if if="[[collapsible]]">
+              <template>
+                <nuxeo-collapsible>
+                  <label slot="heading">[[label]]</label>
+                  <dom-repeat items="{{_visibleBuckets}}">
+                    <template>
+                      <paper-checkbox noink checked="{{item.checked}}" on-change="_computeValues">
+                        [[item.label]] ([[item.docCount]])
+                      </paper-checkbox>
+                    </template>
+                  </dom-repeat>
+                  <span hidden$="[[_hideShowMore(buckets)]]" class="showButton">
+                    <a href="javascript:void(0);" on-tap="_toggleShow">
+                      [[_computeShowMoreLabel(_showAll, i18n)]]
+                    </a>
+                  </span>
+                </nuxeo-collapsible>
+              </template>
+            </dom-if>
           </template>
         </dom-if>
       `;
@@ -73,8 +147,74 @@ import { AggregationBehavior } from './nuxeo-aggregation-behavior.js';
     static get is() {
       return 'nuxeo-checkbox-aggregation';
     }
+
+    static get properties() {
+      return {
+        /**
+         * Make the element collapsible
+         */
+        collapsible: {
+          type: Boolean,
+          value: false,
+        },
+
+        /**
+         * Label of the element
+         */
+        label: {
+          type: String,
+          value: '',
+        },
+
+        _showAll: {
+          type: Boolean,
+          value: false,
+          readOnly: true,
+        },
+
+        _visibleBuckets: {
+          type: Array,
+          value: () => [],
+          readOnly: true,
+        },
+
+        /**
+         * If collapsible, the default visible items.
+         */
+        visibleItems: {
+          type: Number,
+          value: 8,
+        },
+      };
+    }
+
+    static get observers() {
+      return ['_setBuckets(buckets, visibleItems)'];
+    }
+
+    _setBuckets() {
+      if (this.buckets && this.buckets.length > 0) {
+        this._set_visibleBuckets(this.buckets.slice(0, this.visibleItems));
+      }
+    }
+
+    _toggleShow() {
+      this._set_showAll(!this._showAll);
+      if (this._showAll) {
+        this._set_visibleBuckets(this.buckets);
+      } else {
+        this._set_visibleBuckets(this.buckets.slice(0, this.visibleItems));
+      }
+    }
+
+    _computeShowMoreLabel() {
+      return this.i18n((this._showAll && 'checkboxAggregation.showLess') || 'checkboxAggregation.showAll');
+    }
+
+    _hideShowMore() {
+      return this.buckets && this.buckets.length <= this.visibleItems;
+    }
   }
 
-  customElements.define(CheckboxAggregation.is, CheckboxAggregation);
-  Nuxeo.CheckboxAggregation = CheckboxAggregation;
+  customElements.define('nuxeo-checkbox-aggregation', CheckboxAggregation);
 }
