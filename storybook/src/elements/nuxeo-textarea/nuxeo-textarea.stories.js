48deb727275b5fa6f88e0a4d6cc063ae1a24f2d1
Samuel Fialho <samuel.fialho@gmail.com>
2020-10-01 12:41:15 +0100

    ELEMENTS-1243: add nuxeo-textarea to Storybook

---
+++ storybook/src/elements/nuxeo-textarea/nuxeo-textarea.stories.js
@@ -0,0 +1,32 @@
+import { html } from 'lit-html';
+import { storiesOf } from '@storybook/polymer';
+import { boolean, number, text } from '@storybook/addon-knobs';
+import '@nuxeo/nuxeo-ui-elements/widgets/nuxeo-textarea.js';
+
+storiesOf('UI/nuxeo-textarea', module).add('Default', () => {
+  const numberOfRows = number('Number of rows', 3);
+  const label = text('Label', 'Label');
+  const placeholder = text('Placeholder', 'This element represents a multi-line plain-text editing control');
+  const required = boolean('Required', false);
+  const disabled = boolean('Disabled', false);
+  const invalid = boolean('Invalid', false);
+  const readonly = boolean('Read only', false);
+  return html`
+    <style>
+      nuxeo-textarea {
+        max-width: 300px;
+      }
+    </style>
+    <nuxeo-textarea
+      label="${label}"
+      name="description"
+      rows="${numberOfRows}"
+      placeholder="${placeholder}"
+      ?required="${required}"
+      ?disabled="${disabled}"
+      ?invalid="${invalid}"
+      ?readonly="${readonly}"
+    >
+    </nuxeo-textarea>
+  `;
+});
