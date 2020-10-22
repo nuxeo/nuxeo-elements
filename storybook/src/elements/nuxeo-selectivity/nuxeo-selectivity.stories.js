f44ac35bf48f2ac55bc6658f11b3ee8e0b4e8900
Samuel Fialho <samuel.fialho@gmail.com>
2020-10-22 16:06:06 +0100

    ELEMENTS-1236: add nuxeo-selectivity to storybook

---
+++ storybook/src/elements/nuxeo-selectivity/nuxeo-selectivity.stories.js
@@ -0,0 +1,64 @@
+import { html } from 'lit-html';
+import { storiesOf } from '@storybook/polymer';
+import { boolean, number, text } from '@storybook/addon-knobs';
+import '@nuxeo/nuxeo-ui-elements/widgets/nuxeo-selectivity.js';
+
+const data = ['item1', 'item2', 'item3', 'item4'];
+storiesOf('UI/nuxeo-selectivity', module)
+  .add('Single', () => {
+    const label = text('Label', 'Label');
+    const placeholder = text('Placeholder', 'Placeholder');
+    const required = boolean('Required', false);
+    const disabled = boolean('Disabled', false);
+    const invalid = boolean('Invalid', false);
+    const readonly = boolean('Read only', false);
+    const minChars = number('minChars', 0);
+    return html`
+      <style>
+        nuxeo-selectivity {
+          margin: 2rem;
+          max-width: 300px;
+        }
+      </style>
+      <nuxeo-selectivity
+        .data="${data}"
+        label="${label}"
+        placeholder="${placeholder}"
+        ?required="${required}"
+        ?disabled="${disabled}"
+        ?invalid="${invalid}"
+        ?readonly="${readonly}"
+        min-chars="${minChars}"
+      >
+      </nuxeo-selectivity>
+    `;
+  })
+  .add('Multiple', () => {
+    const label = text('Label', 'Label');
+    const placeholder = text('Placeholder', 'Placeholder');
+    const required = boolean('Required', false);
+    const disabled = boolean('Disabled', false);
+    const invalid = boolean('Invalid', false);
+    const readonly = boolean('Read only', false);
+    const minChars = number('minChars', 0);
+    return html`
+      <style>
+        nuxeo-selectivity {
+          margin: 2rem;
+          max-width: 300px;
+        }
+      </style>
+      <nuxeo-selectivity
+        .data="${data}"
+        label="${label}"
+        placeholder="${placeholder}"
+        ?required="${required}"
+        ?disabled="${disabled}"
+        ?invalid="${invalid}"
+        ?readonly="${readonly}"
+        min-chars="${minChars}"
+        multiple
+      >
+      </nuxeo-selectivity>
+    `;
+  });
