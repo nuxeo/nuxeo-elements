bf1b7ed4c1a7ccb25230a278ddfde923bd524e35
Gabriel Barata <gbarata@nuxeo.com>
2022-01-12 15:03:04 +0000

    migrate nuxeo-date to lit

--- ui/widgets/nuxeo-date.js
+++ ui/widgets/nuxeo-date.js
@@ -14,84 +14,77 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
-import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
-import '@nuxeo/nuxeo-elements/nuxeo-element.js';
+import { LitElement, html } from 'lit';
 import { config } from '@nuxeo/nuxeo-elements';
-import { FormatBehavior } from '../nuxeo-format-behavior.js';
-import { I18nBehavior } from '../nuxeo-i18n-behavior.js';
+import { formatDate, formatDateTime } from '../nuxeo-format.js';
+import { I18nController } from '../controllers/nuxeo-i18n-controller.js';
 import './nuxeo-tooltip.js';
 
-{
-  /**
-   * An element to show a datetime formatted with a tooltip. Format maybe relative to now
-   * or [moment Format](http://momentjs.com/docs/#/displaying/format/).
-   *
-   * Example:
-   *
-   *     <nuxeo-date datetime="2016-01-06T00:00:00.000Z"></nuxeo-date>
-   *     <nuxeo-date datetime="2016-01-06T00:00:00.000Z" format="relative"></nuxeo-date>
-   *
-   * @appliesMixin Nuxeo.I18nBehavior
-   * @appliesMixin Nuxeo.FormatBehavior
-   * @memberof Nuxeo
-   * @demo demo/nuxeo-date/index.html
-   */
-  class Date extends mixinBehaviors([I18nBehavior, FormatBehavior], Nuxeo.Element) {
-    static get template() {
-      return html`
-        <span id="datetime" hidden$="[[!datetime]]">[[formatDate(datetime, format, timezone)]]</span>
-        <nuxeo-tooltip for="datetime" hidden$="[[_producesSameDateFormat(datetime, format, tooltipFormat, timezone)]]">
-          [[formatDateTime(datetime, tooltipFormat, timezone)]]
-        </nuxeo-tooltip>
-      `;
-    }
-
-    static get is() {
-      return 'nuxeo-date';
-    }
-
-    static get properties() {
-      return {
-        /*
-         * A string with the date and time to be shown
-         */
-        datetime: {
-          type: String,
-        },
-
-        /*
-         * The format of datetime output text representation. Options are "relative" or the
-         * [moment Format](http://momentjs.com/docs/#/displaying/format/).
-         */
-        format: String,
-
-        /*
-         * The format of datetime output text tooltip. Options are "relative" or the
-         * [moment Format](http://momentjs.com/docs/#/displaying/format/).
-         */
-        tooltipFormat: String,
-
-        /**
-         * The name of the timezone where the user is considered to be, according to the IANA tz database.
-         * Currently valid values are:
-         * - empty: local time will be used, as read from the browser (this is the default)
-         * - Etc/UTC: time specified by the user is assumed to be in UTC
-         */
-        timezone: {
-          type: String,
-          value() {
-            return config.get('timezone');
-          },
-        },
-      };
-    }
-
-    _producesSameDateFormat(datetime, format, tooltipFormat, timezone) {
-      return this.formatDate(datetime, format, timezone) === this.formatDateTime(datetime, tooltipFormat, timezone);
-    }
+/**
+ * An element to show a datetime formatted with a tooltip. Format maybe relative to now
+ * or [moment Format](http://momentjs.com/docs/#/displaying/format/).
+ *
+ * Example:
+ *
+ *     <nuxeo-date datetime="2016-01-06T00:00:00.000Z"></nuxeo-date>
+ *     <nuxeo-date datetime="2016-01-06T00:00:00.000Z" format="relative"></nuxeo-date>
+ *
+ * @appliesMixin Nuxeo.I18nBehavior
+ * @appliesMixin Nuxeo.FormatBehavior
+ * @memberof Nuxeo
+ * @demo demo/nuxeo-date/index.html
+ */
+class Date extends LitElement {
+  static get properties() {
+    return {
+      /*
+       * A string with the date and time to be shown
+       */
+      datetime: { type: String },
+      /*
+       * The format of datetime output text representation. Options are "relative" or the
+       * [moment Format](http://momentjs.com/docs/#/displaying/format/).
+       */
+      format: { type: String },
+      /*
+       * The format of datetime output text tooltip. Options are "relative" or the
+       * [moment Format](http://momentjs.com/docs/#/displaying/format/).
+       */
+      tooltipFormat: { type: String },
+      /**
+       * The name of the timezone where the user is considered to be, according to the IANA tz database.
+       * Currently valid values are:
+       * - empty: local time will be used, as read from the browser (this is the default)
+       * - Etc/UTC: time specified by the user is assumed to be in UTC
+       */
+      timezone: { type: String },
+    };
   }
 
-  customElements.define(Date.is, Date);
-  Nuxeo.Date = Date;
+  constructor() {
+    super();
+    this.i18nController = new I18nController(this);
+    this.timezone = config.get('timezone');
+  }
+
+  render() {
+    return html`
+      <span id="datetime" ?hidden=${!this.datetime}
+        >${formatDate(this.datetime, this.format, this.timezone, this.i18n)}</span
+      >
+      <nuxeo-tooltip
+        for="datetime"
+        ?hidden=${this._producesSameDateFormat(this.datetime, this.format, this.tooltipFormat, this.timezone)}
+      >
+        ${formatDateTime(this.datetime, this.tooltipFormat, this.timezone)}
+      </nuxeo-tooltip>
+    `;
+  }
+
+  _producesSameDateFormat(datetime, format, tooltipFormat, timezone) {
+    return formatDate(datetime, format, timezone) === formatDateTime(datetime, tooltipFormat, timezone);
+  }
 }
+
+customElements.define('nuxeo-date', Date);
+Nuxeo.Date = Date;
