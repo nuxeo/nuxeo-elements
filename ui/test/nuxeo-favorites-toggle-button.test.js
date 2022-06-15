5c3d2eb20c6652c109907fcd5676507e83bb7d8f
Nisha-QA <nisha.roy@contractors.onbase.com>
2022-06-15 15:30:22 +0530

    Fixing the linting issue again

--- ui/test/nuxeo-favorites-toggle-button.test.js
+++ ui/test/nuxeo-favorites-toggle-button.test.js
@@ -14,7 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import { fixture, fixtureCleanup, html, login, tap, waitChanged } from '@nuxeo/testing-helpers';
+import { fixture, html, login, tap, waitChanged } from '@nuxeo/testing-helpers';
 import '../actions/nuxeo-favorites-toggle-button.js';
 
 suite('nuxeo-favorites-toggle-button', () => {
