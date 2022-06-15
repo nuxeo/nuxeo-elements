1c2b2f125a801a78e4827fe182fbf34917c1f2e1
Diksha Verma <diksha.verma@globallogic.com>
2022-06-15 20:55:30 +0530

    Workshop Dev Add user confirmation to toggle favorite action

--- ui/actions/nuxeo-favorites-toggle-button.js
+++ ui/actions/nuxeo-favorites-toggle-button.js
@@ -147,25 +147,29 @@ import '../nuxeo-button-styles.js';
     _toggle() {
       if (!this.favorite) {
         this.$.opAdd.execute().then(() => {
-          this.dispatchEvent(
-            new CustomEvent('added-to-favorites', {
-              composed: true,
-              bubbles: true,
-              detail: { doc: this.document },
-            }),
-          );
-          this._setFavorite(true);
+          if (window.confirm('Are you sure you want to add this document to Favorites?') === true) {
+            this.dispatchEvent(
+              new CustomEvent('added-to-favorites', {
+                composed: true,
+                bubbles: true,
+                detail: { doc: this.document },
+              }),
+            );
+            this._setFavorite(true);
+          }
         });
       } else {
         this.$.opRemove.execute().then(() => {
-          this.dispatchEvent(
-            new CustomEvent('removed-from-favorites', {
-              composed: true,
-              bubbles: true,
-              detail: { doc: this.document },
-            }),
-          );
-          this._setFavorite(false);
+          if (window.confirm('Are you sure you want to remove this document from Favorites?') === true) {
+            this.dispatchEvent(
+              new CustomEvent('removed-from-favorites', {
+                composed: true,
+                bubbles: true,
+                detail: { doc: this.document },
+              }),
+            );
+            this._setFavorite(false);
+          }
         });
       }
     }
