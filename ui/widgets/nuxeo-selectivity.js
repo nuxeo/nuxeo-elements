70cdd6dbc6455016f7a08ca3e380ebf934a5c968
swarnadipa-dev <swarnadipa.choudhury@globallogic.com>
2023-04-26 11:22:55 +0530

    test

--- ui/widgets/nuxeo-selectivity.js
+++ ui/widgets/nuxeo-selectivity.js
@@ -6839,7 +6839,7 @@ typedArrayTags[weakMapTag] = false;
             box-shadow: 0 1px 5px 1px rgba(0, 0, 0, 0.15), 0 10px 16px 0 rgba(0, 0, 0, 0.2);
             z-index: 9999;
             word-break: break-all;
-            position: sticky;
+            position: absolute;
             border-radius: 4px;
           }
 
