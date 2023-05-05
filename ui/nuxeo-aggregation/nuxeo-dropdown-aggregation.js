b19ae31d5148c5ee941b1284fd6c2da3d4c118ed
rahuljain-dev <rahul.jain@contractors.onbase.com>
2023-05-05 10:15:19 +0530

    fix-ELEMENTS-1608-Fix-display-of-selected-filters-in-the-column-headers-3.1.x

--- ui/nuxeo-aggregation/nuxeo-dropdown-aggregation.js
+++ ui/nuxeo-aggregation/nuxeo-dropdown-aggregation.js
@@ -46,6 +46,7 @@ import { AggregationBehavior } from './nuxeo-aggregation-behavior.js';
           :host {
             display: block;
             width: 100%;
+            height: 70%;
           }
 
           [hidden] {
