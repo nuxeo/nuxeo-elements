6eaecb024c156d328a12d07f64fae9e9a57269a6
rahuljain-dev <rahul.jain@contractors.onbase.com>
2023-06-26 12:12:18 +0530

    ELEMENTS-1641: Use feature flag for view vs download

--- core/nuxeo-document.js
+++ core/nuxeo-document.js
@@ -186,21 +186,26 @@ import './nuxeo-resource.js';
         const fileContent = documentDataProperties && documentDataProperties['file:content'];
         const files = documentDataProperties && documentDataProperties['files:files'];
         if (
-          (fileContent && fileContent.data && !this.isCDNEnabled(fileContent.data)) ||
-          (files &&
-            files.length > 0 &&
-            files.every((item) => item.file && item.file.data && !this.isCDNEnabled(item.file.data)))
+          ((fileContent && fileContent.data) ||
+            (files && files.length > 0 && files.every((item) => item.file && item.file.data))) &&
+          !this.isUseDirectUploadS3Enabled()
         ) {
           this.setDocumentViewDownloadProp(); // appends clientReason parameter for view vs download implementation
         }
       });
     }
 
-    /* This code will be optimized to replace the hardcoded url to use a more generic approach as part of
-        https://jira.nuxeo.com/browse/ELEMENTS-1641 in the upcoming release */
-
-    isCDNEnabled(hostUrl) {
-      return hostUrl.includes('cloudfront.net');
+    isUseDirectUploadS3Enabled() {
+      if (
+        Nuxeo &&
+        Nuxeo.UI &&
+        Nuxeo.UI.config &&
+        Nuxeo.UI.config.s3 &&
+        String(Nuxeo.UI.config.s3.useDirectUpload) === 'true'
+      ) {
+        return true;
+      }
+      return false;
     }
 
     /* Fetch the document. */
