871cfe742aee7c7ee434cd8c3345c1fc3a6bfe33
rahuljain-dev <rahul.jain@contractors.onbase.com>
2024-10-15 23:47:57 +0530

    Review comments incorporated

--- ui/widgets/nuxeo-tag-suggestion.js
+++ ui/widgets/nuxeo-tag-suggestion.js
@@ -237,7 +237,7 @@ import { escapeHTML } from './nuxeo-selectivity.js';
     }
 
     _newEntryFormatter(term) {
-      term = term.toLowerCase();
+      term = term?.toLowerCase();
       return { id: term, displayLabel: term, newTag: true };
     }
 
