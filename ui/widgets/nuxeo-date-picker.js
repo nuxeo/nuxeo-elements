6705f6693002d196cf9b883b4702ccdfc50714b1
swarnadipa-dev <swarnadipa.choudhury@globallogic.com>
2024-11-12 11:28:11 +0530

    test commit

--- ui/widgets/nuxeo-date-picker.js
+++ ui/widgets/nuxeo-date-picker.js
@@ -84,8 +84,6 @@ import { I18nBehavior } from '../nuxeo-i18n-behavior.js';
           type: Boolean,
           value: false,
         },
-
-        //,,m,
         /*
          * The first day of week is to be displayed (e.g. `"Sunday -> 0"`, ... `"Saturday -> 6"`).
          * By default, it will be set according the locale.
