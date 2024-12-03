9968929cce14a8f1c657da5123e64bb27a897d52
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2024-12-03 10:37:28 +0530

    ELEMENTS-1772: fix error wih saved search using deleted authors

--- ui/widgets/nuxeo-selectivity.js
+++ ui/widgets/nuxeo-selectivity.js
@@ -6811,6 +6811,11 @@ typedArrayTags[weakMapTag] = false;
           type: Object,
           value: null,
         },
+
+        _backupData: {
+          type: Array,
+          value: [],
+        }
       };
     }
 
@@ -7306,6 +7311,12 @@ typedArrayTags[weakMapTag] = false;
     }
 
     _resolveEntry(entry) {
+      if (this.data && this.data.length > 1) {
+        this._backupData = this.data;
+      }
+      if (this.data && this._backupData && this.data.length < this._backupData.length) {
+        this.data = this._backupData;
+      }
       if (this.data) {
         for (let i = 0; i < this.data.length; i++) {
           if (this.idFunction(this.data[i]) === entry) {
