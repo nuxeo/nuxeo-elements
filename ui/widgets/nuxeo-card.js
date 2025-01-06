6696de5026b98e8ad3f0f6773c97a5d3cbbabc2e
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2025-01-06 14:33:04 +0530

    WEBUI-1616: rtl support

--- ui/widgets/nuxeo-card.js
+++ ui/widgets/nuxeo-card.js
@@ -72,7 +72,7 @@ import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
             cursor: pointer;
             @apply --nuxeo-link-hover;
           }
-          
+
           :host([dir="rtl"]) .header .icon {
             margin-left: 8px;
             margin-right: 0;
