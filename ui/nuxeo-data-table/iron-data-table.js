f584c340f1803c7e78993832a701bb46fd003721
Swarnadipa Choudhury <swarnadipa.choudhury@globallogic.com>
2025-04-22 10:17:23 +0530

    added logic to set height of wrapper dynamically

--- ui/nuxeo-data-table/iron-data-table.js
+++ ui/nuxeo-data-table/iron-data-table.js
@@ -221,7 +221,6 @@ import '../nuxeo-button-styles.js';
           }
 
           .table-wrapper {
-            height: 350px;
             overflow-y: scroll;
             position: relative;
           }
@@ -527,6 +526,14 @@ import '../nuxeo-button-styles.js';
           type: String,
           value: '',
         },
+        /**
+         * Provides a fixed height of the wrapper div containing the iron-list, where there is a limit to
+         * the number of items displayed at a time on the UI.
+         */
+        wrapperHeight: {
+          type: String,
+          value: '',
+        },
       };
     }
 
@@ -600,11 +607,12 @@ import '../nuxeo-button-styles.js';
       this.setAttribute('role', 'table');
       this.setAttribute('aria-multiselectable', this.multiSelection);
       this.setAttribute('aria-label', this.captionText);
-      if (this.classList.contains('wrap-table')) {
+      if (this.wrapperHeight) {
         const list = this.shadowRoot.querySelector('iron-list');
         if (list && !list.parentElement.classList.contains('table-wrapper')) {
           const wrapper = document.createElement('div');
           wrapper.classList.add('table-wrapper');
+          wrapper.setAttribute('style', `height: ${this.wrapperHeight}`);
           list.parentElement.insertBefore(wrapper, list);
           wrapper.appendChild(list);
         }
