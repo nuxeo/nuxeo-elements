edd5b3dc8072d5e37ae81675bc6604967ea8f9c2
Swarnadipa Choudhury <swarnadipa.choudhury@globallogic.com>
2025-04-22 10:08:51 +0530

    added wrapperHeight property which is exposed to components using iron-list to provide a specific wrapper height

--- ui/nuxeo-data-table/iron-data-table.js
+++ ui/nuxeo-data-table/iron-data-table.js
@@ -221,7 +221,6 @@ import '../nuxeo-button-styles.js';
           }
 
           .table-wrapper {
-            height: 350px;
             overflow-y: scroll;
             position: relative;
           }
@@ -492,6 +491,15 @@ import '../nuxeo-button-styles.js';
           value: false,
         },
         /**
+         * Provides a fixed height of the wrapper div containing the iron-list, where there is a limit to
+         * the number of items displayed at a time on the UI.
+         */
+        wrapperHeight: {
+          type: String,
+          value: '',
+        },
+        /**
+         *
          * If enabled, it allows to select multiple documents and apply an action on them,
          * like moving them to trash, to favorites or to a collection.
          */
@@ -600,11 +608,12 @@ import '../nuxeo-button-styles.js';
       this.setAttribute('role', 'table');
       this.setAttribute('aria-multiselectable', this.multiSelection);
       this.setAttribute('aria-label', this.captionText);
-      if (this.classList.contains('wrap-table')) {
+      if (this.wrapperHeight) {
         const list = this.shadowRoot.querySelector('iron-list');
         if (list && !list.parentElement.classList.contains('table-wrapper')) {
           const wrapper = document.createElement('div');
           wrapper.classList.add('table-wrapper');
+          wrapper.setAttribute('style', `height: ${  this.wrapperHeight}`);
           list.parentElement.insertBefore(wrapper, list);
           wrapper.appendChild(list);
         }
