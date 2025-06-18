6877285fb3c9f03d183c5877d8dda2eb2f10b034
AnilKumarVanga <Vanga.Kumar@hyland.com>
2025-06-18 12:42:50 +0530

    fix:testing

--- core/nuxeo-element.js
+++ core/nuxeo-element.js
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-
+// testing
 /**
  * Nuxeo namespace
  */
