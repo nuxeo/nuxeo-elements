eda0ceb1f47b3f4dbc787b64313a34a6a71d5df2
Nishant0928 <Nishant.Singh@contractors.onbase.com>
2025-07-10 14:35:31 +0530

    minor changes

--- ui/nuxeo-user-group-management/nuxeo-user-group-permissions-table.js
+++ ui/nuxeo-user-group-management/nuxeo-user-group-permissions-table.js
@@ -49,7 +49,7 @@ import '../nuxeo-button-styles.js';
    * @memberof Nuxeo
    */
   class UserGroupPermissionsTable extends mixinBehaviors([I18nBehavior], Nuxeo.Element) {
-    static get template() {
+    static get template () {
       return html`
         <style include="iron-flex iron-flex-factors nuxeo-button-styles">
           :host {
@@ -293,13 +293,16 @@ import '../nuxeo-button-styles.js';
     }
 
     connectedCallback() {
-      if (typeof super.connectedCallback === 'function') {
+      try {
         super.connectedCallback();
+      } catch (e) {
+        // No-op if super.connectedCallback is not defined
       }
-
+      // Initial fetch
       if (this.entity) {
         this._fetchPermissions();
       }
+      // Setup IntersectionObserver to detect when visible again
       this._intersectionObserver = new IntersectionObserver(
         (entries) => {
           entries.forEach((entry) => {
@@ -309,7 +312,7 @@ import '../nuxeo-button-styles.js';
           });
         },
         {
-          threshold: 0.1,
+          threshold: 0.1, // Trigger when at least 10% visible
         },
       );
 
@@ -320,15 +323,12 @@ import '../nuxeo-button-styles.js';
       if (!this.entity) {
         return;
       }
+      // Defer to ensure <nuxeo-operation> is fully attached
       requestAnimationFrame(() => {
         if (!this.$.permissions) {
           console.warn('Permissions operation not ready');
           return;
         }
-        if (this._abortController) {
-          this._abortController.abort();
-        }
-        this._abortController = new AbortController();
         this.$permissions.params = {
           query: `${'SELECT * FROM Document WHERE ecm:mixinType != "HiddenInNavigation"' +
             'AND ecm:isProxy = 0 AND ecm:isVersion = 0 ' +
@@ -429,8 +429,10 @@ import '../nuxeo-button-styles.js';
       if (this._intersectionObserver) {
         this._intersectionObserver.disconnect();
       }
-      if (typeof super.disconnectedCallback === 'function') {
+      try {
         super.disconnectedCallback();
+      } catch (e) {
+        // No-op if super.connectedCallback is not defined
       }
     }
 
