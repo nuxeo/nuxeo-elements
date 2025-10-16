e8a92f0e8be4b0b9eeddd382351da41dd52a35ed
Nishant0928 <113914009+Nishant0928@users.noreply.github.com>
2025-10-16 21:16:56 +0530

    ELEMENTS-1869: prevent from clicking multiple times on the add comment button 3.1.x (#1176)
    * ELEMENTS-1869: prevent from clicking multiple times on the add comment button
    
    * ELEMENTS-1869: fixing lint errors
    
    * ELEMEMTS-1869: fixing linting error
    
    * ELEMENTS-1869: prevent from clicking multiple times on the add comment button
    
    * ELEMENTS-1869: prevent from clicking multiple times on the add comment button
    
    * ELEMENTS-1869: fix lint issues
    
    * ELEMENTS-1869:: fix Eslint issue
    
    * ELEMENTS-1869: prevent from clicking multiple times on the add comment button
    
    * ELEMENTS-1869: prevent from clicking multiple times on the add comment button
    
    * ELEMENTS-1869: prevent from clicking multiple times on the add comment button
    
    ---------
    
    Co-authored-by: AnilKumarVanga <Vanga.Kumar@hyland.com>

--- ui/nuxeo-document-comments/nuxeo-document-comment.js
+++ ui/nuxeo-document-comments/nuxeo-document-comment.js
@@ -87,6 +87,11 @@ import '../nuxeo-button-styles.js';
             white-space: pre-wrap;
           }
 
+          iron-icon.disabled {
+            pointer-events: none;
+            opacity: 0.4;
+          }
+
           paper-menu-button {
             --paper-menu-button: {
               padding: 0;
@@ -261,7 +266,7 @@ import '../nuxeo-button-styles.js';
                               role="button"
                               aria-label="[[i18n('command.selectComment')]]"
                               name="submit"
-                              class="main-option opaque"
+                              class$="main-option opaque [[_computeDisabledClass(_isSubmitting)]]"
                               icon="check"
                               on-tap="_submitComment"
                               on-keydown="_submitOnEnter"
@@ -488,9 +493,23 @@ import '../nuxeo-button-styles.js';
       this.set('comment.showFull', true);
     }
 
+    _computeDisabledClass(isSubmitting) {
+      return isSubmitting ? 'disabled' : '';
+    }
+
     _submitComment(e) {
       if (e) {
         e.preventDefault();
+      }
+      if (this._isSubmitting) {
+        return;
+      }
+      this._isSubmitting = true;
+
+      const input = this.$$('#inputContainer');
+      if (!input || this._isBlank(input.value)) {
+        this._isSubmitting = false;
+        return;
       }
       this.$.commentRequest.data = {
         'entity-type': 'comment',
@@ -524,6 +543,9 @@ import '../nuxeo-button-styles.js';
             this.notify({ message: this._computeTextLabel(this.level, 'edition.error') });
             throw error;
           }
+        })
+        .finally(() => {
+          this._isSubmitting = false;
         });
     }
 
