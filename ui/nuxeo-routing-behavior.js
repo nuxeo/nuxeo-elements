d946dd7303b1c32641ed954af916f1a9a15c8a70
AnilKumarVanga <Vanga.Kumar@hyland.com>
2026-05-06 11:29:48 +0530

    removed unwanted code

--- ui/nuxeo-routing-behavior.js
+++ ui/nuxeo-routing-behavior.js
@@ -75,7 +75,7 @@ export const RoutingBehavior = {
    * Returns a computed `urlFor` method, based on the current `router`.
    */
   _computeUrlFor() {
-    return function (...args) {
+    return function(...args) {
       if (this.router) {
         const [route, ...params] = args;
         if (!route) {
@@ -112,7 +112,7 @@ export const RoutingBehavior = {
    * Invokes `router.navigate` to trigger the actual navigation.
    */
   _computeNavigateTo() {
-    return function (...args) {
+    return function(...args) {
       if (this.router) {
         const [route, ...params] = args;
         let path;
