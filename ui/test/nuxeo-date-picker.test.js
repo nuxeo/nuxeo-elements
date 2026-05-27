cf6471dab8bf8541ce34c541bf78c4398b55b28f
vaibhavagarwal4-lab <Vaibhav.Agarwal@hyland.com>
2026-05-27 12:52:06 +0530

    test fix

--- ui/test/nuxeo-date-picker.test.js
+++ ui/test/nuxeo-date-picker.test.js
@@ -15,7 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import { fixture, html } from '@nuxeo/testing-helpers';
+import { fixture, flush, html } from '@nuxeo/testing-helpers';
 import moment from '@nuxeo/moment/min/moment-with-locales.js';
 import '../widgets/nuxeo-date-picker.js';
 
