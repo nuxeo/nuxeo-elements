863ca7a0466ff04e833084a6d1d45d1ab1341dbb
naveen-konda <naveen.konda@hyland.com>
2026-09-07 19:43:34 +0530

    ELEMENTS-2085: Use object spread instead of Object.assign (#1675)

--- ui/nuxeo-data-table/iron-data-table.js
+++ ui/nuxeo-data-table/iron-data-table.js
@@ -1038,7 +1038,9 @@ function hasRowDetailTemplate(node) {
 
     get settings() {
       const sortOrder = Array.isArray(this.sortOrder)
-        ? this.sortOrder.map((entry) => Object.assign({}, entry))
+        ? this.sortOrder.map((entry) => {
+            return { ...entry };
+          })
         : this.sortOrder || null;
 
       const tableSettings = {
